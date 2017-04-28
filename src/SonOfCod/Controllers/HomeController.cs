d44228639de12c9d90589ea37e5bfe00bd4944c1
Nicholas Mead <nixter11@uw.edu>
2017-04-28 16:00:14 -0700

    add carousel on front page

--- src/SonOfCod/Controllers/HomeController.cs
+++ src/SonOfCod/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SonOfCod.ViewModels;
+using SonOfCod.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,28 @@ namespace SonOfCod.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        private readonly CodDbContext _db;
+
+        public HomeController(CodDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Newsletter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Subscribe(Subscriber newSubscriber)
+        {
+            _db.Subscribers.Add(newSubscriber);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ src/SonOfCod/Models/PageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SonOfCod.Models
+{
+    [Table("PageInfo")]
+    public class PageInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NewsletterBody { get; set; }
+        public string LandingBody { get; set; }
+    }
+}
