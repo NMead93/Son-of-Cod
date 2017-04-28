8a7df9505d2a6be343a3b44708981eb43035c1b1
Nicholas Mead <nixter11@uw.edu>
2017-04-28 10:24:13 -0700

    initialize project files and render navbar on layout page

---
+++ src/SonOfCod/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SonOfCod.Models;
+
+namespace SonOfCod.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly CodDbContext _db;
+        private readonly UserManager<Admin> _userManager;
+        private readonly SignInManager<Admin> _signInManager;
+
+        public AccountController(UserManager<Admin> userManager, SignInManager<Admin> signInManager, CodDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/SonOfCod/Models/CodDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SonOfCod.Models
+{
+    public class CodDbContext : IdentityDbContext<Admin>
+    {
+        public CodDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
